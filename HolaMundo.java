0fb0d1ab3ca929983fc87cd8cc68e264feb8c514
Jose Marín <jmarinmalagon@gmail.com>
2022-01-11 08:56:44 +0100

    Update HolaMundo.java

--- HolaMundo.java
+++ HolaMundo.java
@@ -3,5 +3,6 @@ public class HolaMundo {
 	public static void main(String[] args){
 		System.out.println("Hola Mundo, Que tal?");
 		System.out.println("Adios!!!" );
+		System.out.println("Bienvenidos!!!" );
 	}
 }
